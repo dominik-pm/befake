0e6addd73c79a3a49095af590b0cdc0fe485348a
hubertoschusch <hubert.schusch@gmail.com>
2023-01-15 19:25:05 +0100

    upper and lowercase dont impair friend posts filtering anymore

--- src/pages/AllFriendPosts.vue
+++ src/pages/AllFriendPosts.vue
@@ -30,7 +30,7 @@ export default {
         },
         filter(name) {
             for (let friendpost of document.getElementsByClassName("friendpost")) {
-                if (!friendpost.innerText.includes(name)) {
+                if (!friendpost.innerText.includes(name.toLowerCase())) {
                     friendpost.style = "display:none"
                 } else {
                     friendpost.style.display = "block"
@@ -48,7 +48,7 @@ export default {
 
 <template>
     <div>
-        <input type="search" id="search" @input="event => {filter(event.target.value)}" class="m-auto mb-5 text-black block p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Filter user">
+        <input type="search" id="search" @input="event => {filter(event.target.value)}" class="m-auto mb-5 block p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Filter user">
     </div>
     <div v-for="(friend, index) in friends" class="m-auto">
         <FriendPosts class="friendpost" :user-name="friend['username']"/>
