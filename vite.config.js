c2b9c8d03016a33a5ddd06a0e6929ddda877e779
hubertoschusch <hubert.schusch@gmail.com>
2023-01-18 22:11:22 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,11 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import basicSsl from '@vitejs/plugin-basic-ssl'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), basicSsl()],
-  server: {
-    https: true
-  },
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
