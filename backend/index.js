eb62b800b01b5b940ef13759801068984da66868
hubertoschusch <hubert.schusch@gmail.com>
2023-02-03 18:51:30 +0100

    added new local proxy

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var http = require('http')
 var https = require('https')
-var config = require('./../src/data/config.js')
+import config from ('./../src/data/config.js')
 
 try {
   var privateKey  = fs.readFileSync('/etc/ssl/private/key.pem', 'utf8');
